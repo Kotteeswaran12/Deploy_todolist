468bb5deb1456a249183416767ecd23c48881e30
kotteeswaran12 <kotteeswaran2005@gmail.com>
2025-02-19 12:24:36 +0530

    Fix missing .env issue

--- src/main/java/com/example/todolist_api/todolist_api/Model/Todolist.java
+++ src/main/java/com/example/todolist_api/todolist_api/Model/Todolist.java
@@ -14,7 +14,7 @@ import lombok.Setter;
 public class Todolist {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String list;
